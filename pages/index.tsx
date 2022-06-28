0c6c15a293441dbc156b572b787e5ea0f50b060b
Kacper Malek <kacper.malek0828@gmail.com>
2022-06-28 22:00:43 +0200

    feat: add Head component to display different title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import Head from "next/head";
+
 import AppContainer from "@/components/AppContainer";
 
 export default function HomePage() {
   return (
     <>
+      <Head>
+        <title>Recipe Smith</title>
+      </Head>
       <AppContainer />
     </>
   );

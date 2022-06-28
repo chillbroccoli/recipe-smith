cf16b687db4c97063b102e764a8b2fb9e8b0888f
Kacper Malek <kacper.malek0828@gmail.com>
2022-06-28 21:59:43 +0200

    feat: add api lib file, export recipes and recipe fetching functions

---
+++ lib/api.ts
@@ -0,0 +1,16 @@
+export const fetchRecipes = async (searchQuery: string) => {
+  const resultsResponse = await fetch(
+    `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${searchQuery}&number=100&ranking=2&ignorePantry=true&apiKey=${process.env.API_KEY}`
+  );
+  const results = await resultsResponse.json();
+  return results;
+};
+
+export const fetchRecipe = async (id: string) => {
+  const resultResponse = await fetch(
+    `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&apiKey=${process.env.API_KEY}`
+  );
+  const result = await resultResponse.json();
+
+  return result;
+};

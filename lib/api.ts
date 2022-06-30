46169ca508bc47b9966faa661fc78910879b75f8
Kacper Malek <kacper.malek0828@gmail.com>
2022-06-30 16:40:21 +0200

    refactor: use NEXT_PUBLIC_API_KEY instead of API_KEY

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 export const fetchRecipes = async (searchQuery: string) => {
   const resultsResponse = await fetch(
-    `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${searchQuery}&number=100&ranking=2&ignorePantry=true&apiKey=${process.env.API_KEY}`
+    `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${searchQuery}&number=100&ranking=2&ignorePantry=true&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`
   );
   const results = await resultsResponse.json();
   return results;
@@ -8,7 +8,7 @@ export const fetchRecipes = async (searchQuery: string) => {
 
 export const fetchRecipe = async (id: string) => {
   const resultResponse = await fetch(
-    `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&apiKey=${process.env.API_KEY}`
+    `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=true&apiKey=${process.env.NEXT_PUBLIC_API_KEY}`
   );
   const result = await resultResponse.json();
 

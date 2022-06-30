1a86bd9ce235df82290eac4a5fe304b09e02295c
Kacper Malek <kacper.malek0828@gmail.com>
2022-06-30 12:14:24 +0200

    feat: adjust background, background colors, and a link with a title

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,11 +1,22 @@
+import Link from "next/link";
+
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <div className="w-full min-h-screen bg-gradient-to-br from-green-500/20 via-emerald-500/20 to-teal-500/20">
-      <main className="w-full min-h-screen">{children}</main>
+    <div className="w-full min-h-screen bg-gradient-to-r lg:flex lg:items-center lg:justify-center from-green-500 via-emerald-500 to-teal-500 lg:py-20">
+      <div className="bg-white w-full min-h-screen lg:min-h-[80vh] lg:w-5/6 2xl:w-3/5 rounded-lg">
+        <div className="w-full py-4 px-4">
+          <Link href="/">
+            <h1 className="logo-title text-center text-lg cursor-pointer">
+              Recipe Smith
+            </h1>
+          </Link>
+        </div>
+        <main>{children}</main>
+      </div>
     </div>
   );
 }
